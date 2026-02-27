8ae7179802646b68b83ec43bdd545ec7a965cac9
Kian O'Connor <kianoconnor@MacBook-Pro.local>
2026-02-26 20:38:19 -0800

    Revert "Add OpenSubsidies to sister site footer"
    This reverts commit 32c1673b06ea31ffec869a4ff865c22b20864b5c.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -67,7 +67,6 @@ export default function Footer() {
               <li><a href="https://www.openfeds.org" className="hover:text-white transition-colors">OpenFeds</a></li>
               <li><a href="https://www.openspending.us" className="hover:text-white transition-colors">OpenSpending</a></li>
               <li><a href="https://www.openlobby.us" className="hover:text-white transition-colors">OpenLobby</a></li>
-              <li><a href="https://www.opensubsidies.us" className="hover:text-white transition-colors">OpenSubsidies</a></li>
               <li><a href="https://www.vaccinewatch.org" className="hover:text-white transition-colors">VaccineWatch</a></li>
               <li><a href="https://thedataproject.ai" className="hover:text-white transition-colors">TheDataProject.ai</a></li>
             </ul>
